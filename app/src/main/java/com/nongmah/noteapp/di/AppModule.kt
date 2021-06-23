4b35a0a6e17163ee52450e796263da26d441b75a
Surasak Adulprasertsuk <jojo141a@hotmail.com>
2021-06-23 19:02:43 +0700

    setup Hilt application module

---
+++ app/src/main/java/com/nongmah/noteapp/di/AppModule.kt
@@ -0,0 +1,74 @@
+package com.nongmah.noteapp.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.room.Room
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.nongmah.noteapp.data.local.NotesDatabase
+import com.nongmah.noteapp.data.remote.BasicAuthInterceptor
+import com.nongmah.noteapp.data.remote.NoteApi
+import com.nongmah.noteapp.other.Constants.BASE_URL
+import com.nongmah.noteapp.other.Constants.DATABASE_NAME
+import com.nongmah.noteapp.other.Constants.ENCRYPTED_SHARED_PREF_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNotesDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, NotesDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideProvideNoteDao(db: NotesDatabase) = db.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideBasicAuthInterceptor() = BasicAuthInterceptor()
+
+    @Singleton
+    @Provides
+    fun provideNoteApi(
+        basicAuthInterceptor: BasicAuthInterceptor
+    ): NoteApi {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(basicAuthInterceptor)
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(NoteApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideEncryptedSharedPreferences(
+        @ApplicationContext context: Context
+    ): SharedPreferences {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+        return EncryptedSharedPreferences.create(
+            context,
+            ENCRYPTED_SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
--- app/src/main/java/com/nongmah/noteapp/other/Constants.kt
+++ app/src/main/java/com/nongmah/noteapp/other/Constants.kt
@@ -2,5 +2,9 @@ package com.nongmah.noteapp.other
 
 object Constants {
 
+    const val DATABASE_NAME = "note_db"
+    const val BASE_URL = "http://10.0.2.2:8001"
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
     val IGNORE_AUTH_URLS = listOf("/login", "/register")
 }
