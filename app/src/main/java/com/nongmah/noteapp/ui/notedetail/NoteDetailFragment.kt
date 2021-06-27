c1dc2d9c92bd81f2cdab4db8f9c04830d032c9c3
Surasak Adulprasertsuk <jojo141a@hotmail.com>
2021-06-27 18:41:08 +0700

    update note edit icon to open add/edit note fragment

--- app/src/main/java/com/nongmah/noteapp/ui/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/nongmah/noteapp/ui/notedetail/NoteDetailFragment.kt
@@ -1,7 +1,27 @@
 package com.nongmah.noteapp.ui.notedetail
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.nongmah.noteapp.R
+import com.nongmah.noteapp.databinding.FragmentNoteDetailBinding
 import com.nongmah.noteapp.ui.BaseFragment
 
 class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
+
+    private val args: NoteDetailFragmentArgs by navArgs()
+
+    lateinit var binding: FragmentNoteDetailBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentNoteDetailBinding.bind(view)
+
+        binding.fabEditNote.setOnClickListener {
+            findNavController().navigate(
+                NoteDetailFragmentDirections.actionNoteDetailFragmentToAddEditNoteFragment(args.id)
+            )
+        }
+    }
 }
