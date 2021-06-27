77640ec64fcd9a7ecb0b77ef771ce524ba9e7b08
Surasak Adulprasertsuk <jojo141a@hotmail.com>
2021-06-27 17:48:07 +0700

    add insert note feature

--- app/src/main/java/com/nongmah/noteapp/ui/addeditnote/AddEditNoteFragment.kt
+++ app/src/main/java/com/nongmah/noteapp/ui/addeditnote/AddEditNoteFragment.kt
@@ -1,7 +1,67 @@
 package com.nongmah.noteapp.ui.addeditnote
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.nongmah.noteapp.R
+import com.nongmah.noteapp.data.local.entities.Note
+import com.nongmah.noteapp.databinding.FragmentAddEditNoteBinding
+import com.nongmah.noteapp.other.Constants.DEFAULT_NOTE_COLOR
+import com.nongmah.noteapp.other.Constants.KEY_LOGGED_IN_EMAIL
+import com.nongmah.noteapp.other.Constants.NO_EMAIL
 import com.nongmah.noteapp.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AddEditNoteFragment : BaseFragment(R.layout.fragment_add_edit_note) {
+
+    private val viewModel: AddEditNoteViewModel by viewModels()
+
+    private val args: AddEditNoteFragmentArgs by navArgs()
+
+    lateinit var binding: FragmentAddEditNoteBinding
+
+    private var currentNote: Note? = null
+    private var currentNoteColor = DEFAULT_NOTE_COLOR
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAddEditNoteBinding.bind(view)
+        if (args.id.isNotEmpty()) {
+            viewModel.getNoteById(args.id)
+            subscribeToObservers()
+        }
+    }
+
+    private fun subscribeToObservers() {
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveNote()
+    }
+
+    private fun saveNote() {
+        val authEmail = sharedPref.getString(KEY_LOGGED_IN_EMAIL, NO_EMAIL) ?: NO_EMAIL
+
+        val title = binding.etNoteTitle.text.toString()
+        val content = binding.etNoteContent.text.toString()
+        if (title.isNotEmpty() || content.isNotEmpty()) {
+            return
+        }
+        val date = System.currentTimeMillis()
+        val color = currentNoteColor
+        val id = currentNote?.id ?: UUID.randomUUID().toString()
+        val owners = currentNote?.owners ?: listOf(authEmail)
+        val note = Note(title, content, date, owners, color, id = id)
+        viewModel.insertNote(note)
+    }
 }
--- app/src/main/java/com/nongmah/noteapp/ui/addeditnote/AddEditNoteViewModel.kt
+++ app/src/main/java/com/nongmah/noteapp/ui/addeditnote/AddEditNoteViewModel.kt
@@ -1,6 +1,35 @@
 package com.nongmah.noteapp.ui.addeditnote
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nongmah.noteapp.data.local.entities.Note
+import com.nongmah.noteapp.other.Event
+import com.nongmah.noteapp.other.Resource
+import com.nongmah.noteapp.repositories.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AddEditNoteViewModel : ViewModel() {
+@HiltViewModel
+class AddEditNoteViewModel @Inject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    private val _note = MutableLiveData<Event<Resource<Note>>>()
+    val note: LiveData<Event<Resource<Note>>> = _note
+
+    fun insertNote(note: Note) = GlobalScope.launch {
+        repository.insertNote(note)
+    }
+
+    fun getNoteById(noteID: String) = viewModelScope.launch {
+        _note.postValue(Event(Resource.loading(null)))
+        val note = repository.getNoteById(noteID)
+        note?.let {
+            _note.postValue(Event(Resource.success(it)))
+        } ?: _note.postValue(Event(Resource.error("Note not found", null)))
+    }
 }
